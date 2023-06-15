0d68d82602b7b6b029800cf4cffe39a5d571146c
IvanKahinga <ikahinga@gmail.com>
2023-06-16 01:39:15 +0300

    Completed experience page

---
+++ src/components/Contact.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div name = "contact" className=' w-full h-screen bg-gradient-to-b from-black to-gray-800 text-white'>
+        <div className='flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full '>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
+                    Contact
+                </p>
+                <p className='py-6'> Get in touch</p>
+            </div>
+            <div className='flex justify-center items-center'>
+                <form action='https://getform.io/f/fa226873-ba2e-401e-a16c-e7f889fb9b15' method='POST' className='flex flex-col w-full md:w-1/2'>
+                <input type="text" name='name ' placeholder='Enter Your name..' className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' />
+                <input type="text" name='email ' placeholder='Enter Your email..' className='p-2 my-4 bg-transparent border-2 rounded-md text-white focus:outline-none' />
+                <textarea name='message' placeholder='Leave a message here...' rows="10" className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' />
+                <button className='text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300'>Lets talk</button>
+
+
+
+                </form>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import HeroImage from "../assets/heroImage.png";
 import { RiArrowRightSLine } from "react-icons/ri";
+import {Link} from "react-scroll";
 
 function Home() {
   return (
@@ -19,12 +20,12 @@ function Home() {
             technologies like React, Tailwind, Ruby on Rails , and PostgreSQL.
           </p>
           <div>
-            <button className=" group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+            <Link to="portfolio" smooth duration={500} className=" group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
               Portfolio
               <span className="group-hover:rotate-90 duration-300">
                 <RiArrowRightSLine size={20} className="ml-1" />
               </span>
-            </button>
+            </Link>
           </div>
         </div>
         <div>
