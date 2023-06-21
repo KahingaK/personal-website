4401fab9c18beaf0f2fc2860ee415f29ae34bb92
IvanKahinga <ikahinga@gmail.com>
2023-06-21 12:38:39 +0300

    Sorted overlapping styling

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -57,9 +57,9 @@ function Experience() {
   return (
     <div
       name="experience"
-      className=" bg-gradient-to-b from-gray-800 to-black w-full h-screen"
+      className=" bg-gradient-to-b from-gray-800 to-black w-full"
     >
-      <div className=" max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white ">
+      <div className="container px-16 py-8 mx-auto p-4 flex flex-col justify-center w-full text-white ">
         <div>
           <p className="text-4xl font-bold inline border-b-4 border-gray-500 p-1">
             My Technologies
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,15 +3,16 @@ import HeroImage from "../assets/heroImage.png";
 import { RiArrowRightSLine } from "react-icons/ri";
 import {Link} from "react-scroll";
 
+
 function Home() {
   return (
     <div
       name="home"
-      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 "
+      className="w-full bg-gradient-to-b from-black via-black to-gray-800 "
     >
-      <div className=" max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-        <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+      <div className=" container mx-auto flex flex-col items-center justify-center px-4 py-24 md:flex-row">
+        <div className=" px-16 flex flex-col justify-center h-full">
+          <h2 className=" text-4xl sm:text-7xl font-bold text-white">
             I am a full stack developer
           </h2>
           <p className=" text-gray-500 py-4 max-w-md">
